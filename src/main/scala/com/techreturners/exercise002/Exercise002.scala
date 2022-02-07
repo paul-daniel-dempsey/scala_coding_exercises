f81a699f290fe91e0a31c9d41b303508759ad677
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-02-07 17:42:34 +0000

    Implemented FirstTest 002 - isFromManchester CanWatchFilm {Shortened}

--- src/main/scala/com/techreturners/exercise002/Exercise002.scala
+++ src/main/scala/com/techreturners/exercise002/Exercise002.scala
@@ -2,8 +2,8 @@ package com.techreturners.exercise002
 
 class Exercise002 {
 
-  def isFromManchester(person: Person): Boolean = if (person.city == "Manchester") true else false
+  def isFromManchester(person: Person): Boolean =  person.city == "Manchester"
 
-  def canWatchFilm(person: Person, ageLimit: Int): Boolean = if (person.age >= ageLimit) true else false
+  def canWatchFilm(person: Person, ageLimit: Int): Boolean = person.age >= ageLimit
 
 }
